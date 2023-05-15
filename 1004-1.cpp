9b14ba397f1dd07b5ba3cd271c038bd948c67b89
Will <LuciferM999999@gmail.com>
2023-05-15 18:29:10 +0800

    1004 另一种写法

--- 1004-1.cpp
+++ 1004-1.cpp
@@ -7,10 +7,31 @@
 
 using namespace std;
 
+struct Stu {
+    string name;
+    string id;
+    unsigned int score;
+    bool operator<(const Stu &s2) const {
+        return score > s2.score;
+    }
+};
+// https://stackoverflow.com/questions/37301160/invalid-operands-to-binary-expression-const-and-const
+//bool StuCompare(Stu &s1, Stu &s2) {
+//    return s1.score > s2.score;
+//}
+
 int main()
 {
-
-//    cout << nameh << " " << idh << endl;
-//    cout << namel << " " << idl << endl;
+    int n; cin >> n;
+    deque<Stu> res;
+    while (n--) {
+        Stu stu;
+        cin >> stu.name >> stu.id >> stu.score;
+        res.push_back(stu);
+    }
+//    sort(res.begin(), res.end(), StuCompare);
+    sort(res.begin(), res.end());
+    cout << res.front().name << " " << res.front().id << endl;
+    cout << res.back().name << " " << res.back().id << endl;
     return 0;
 }
--- 1005.cpp
+++ 1005.cpp
@@ -6,7 +6,10 @@
 using namespace std;
 
 int main() {
-    unsigned short int n;
+    unsigned short int n; cin >> n;
+    for (int i = 0; i < n; ++i) {
+
+    }
     while (cin >>n) {
         while (n!=1) {
             if (n&1) n = 3 * n + 1, n /= 2;
@@ -17,3 +20,16 @@ int main() {
     }
     return 0;
 }
+
+//int main() {
+//    unsigned short int n;
+//    while (cin >>n) {
+//        while (n!=1) {
+//            if (n&1) n = 3 * n + 1, n /= 2;
+//            else n /= 2;
+//            cout << n << " ";
+//        }
+//        cout << endl;
+//    }
+//    return 0;
+//}
